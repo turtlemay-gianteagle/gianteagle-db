1d5e4d097989861dc870f1559d83c92ed86e2c16
Turtlemay <am@trtl.in>
2021-06-02 00:35:25 -0400

    Convert indentation to tabs.

--- Foyfile.js
+++ Foyfile.js
@@ -6,33 +6,33 @@ const schemaJson = require("turtlemay-cashier-db-schema/schema.json");
 const BUILD_DIR = `${__dirname}/build`;
 
 task("build", ["validate"], async (ctx) => {
-  await fs.outputJson(`${BUILD_DIR}/data.json`, buildData());
+	await fs.outputJson(`${BUILD_DIR}/data.json`, buildData());
 });
 
 task("validate", async (ctx) => {
-  const validator = new Validator();
-  try {
-    const result = validator.validate(buildData(), schemaJson);
-    if (result.errors.length > 0) {
-      const [err] = result.errors;
-      throw new Error(`${err.property} ${err.message}`);
-    }
-  } catch (error) {
-    console.error(error);
-  }
+	const validator = new Validator();
+	try {
+		const result = validator.validate(buildData(), schemaJson);
+		if (result.errors.length > 0) {
+			const [err] = result.errors;
+			throw new Error(`${err.property} ${err.message}`);
+		}
+	} catch (error) {
+		console.error(error);
+	}
 });
 
 task("clean", async (ctx) => {
-  await fs.rmrf(BUILD_DIR);
+	await fs.rmrf(BUILD_DIR);
 });
 
 function buildData() {
-  return {
-    name: manifestJson.name,
-    version: manifestJson.version,
-    organization: manifestJson.organization || undefined,
-    items: readData(),
-  };
+	return {
+		name: manifestJson.name,
+		version: manifestJson.version,
+		organization: manifestJson.organization || undefined,
+		items: readData(),
+	};
 }
 
 /**
@@ -40,11 +40,11 @@ function buildData() {
  * @returns {Array} An array of item data objects.
  */
 function readData(dataDir = `${__dirname}/data`) {
-  const fileNames = fs.readdirSync(dataDir);
-  const filePaths = fileNames.map((fileName) => `${dataDir}/${fileName}`);
-  const fileContentsArr = filePaths.map((v) => fs.readFileSync(v, "utf-8"));
-  const itemDataArrays = fileContentsArr.map((fileContents) =>
-    JSON.parse(fileContents)
-  );
-  return [].concat(...itemDataArrays);
+	const fileNames = fs.readdirSync(dataDir);
+	const filePaths = fileNames.map((fileName) => `${dataDir}/${fileName}`);
+	const fileContentsArr = filePaths.map((v) => fs.readFileSync(v, "utf-8"));
+	const itemDataArrays = fileContentsArr.map((fileContents) =>
+		JSON.parse(fileContents)
+	);
+	return [].concat(...itemDataArrays);
 }
